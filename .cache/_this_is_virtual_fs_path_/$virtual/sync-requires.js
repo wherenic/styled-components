425f330436e78a8f0a5701711bc434139eb57368
Nicole <a842322@yara.com>
2021-05-17 07:20:40 +0800

    Test project

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,13 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/nicolelian/Desktop/styled-components/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/Users/nicolelian/Desktop/styled-components/src/pages/404.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/nicolelian/Desktop/styled-components/src/pages/index.js")),
+  "component---src-pages-page-2-js": preferDefault(require("/Users/nicolelian/Desktop/styled-components/src/pages/page-2.js")),
+  "component---src-pages-using-typescript-tsx": preferDefault(require("/Users/nicolelian/Desktop/styled-components/src/pages/using-typescript.tsx"))
+}
+
